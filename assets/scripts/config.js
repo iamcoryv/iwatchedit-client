549f1c532e9b5b8afcc9846594b15a85d5951525
iamcoryv <clvertentes@gmail.com>
2020-05-19 18:15:34 -0400

    Updated heroku link without /

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://infinite-spire-91180.herokuapp.com/',
+  production: 'https://infinite-spire-91180.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
